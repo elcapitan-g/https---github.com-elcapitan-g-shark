1a94308d8123c2f7e473e34a43cd74df12a7a7e7
Your Nameelcapitan-g <gnewell89@gmail.com>
2025-07-20 01:28:52 -1000

    burgherstein

--- routes/sharks.js
+++ routes/sharks.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const sharkController = require('../controllers/sharks');
-//const validateShark = require('../middleware/validate');
+const validateShark = require('../middleware/validate');
 
-router.get('/', sharkController.getAllSharks);
-router.get('/:id', sharkController.getSharkById);
+router.get('/', sharkController.getAll);
+router.get('/:id', sharkController.getSingle);
 router.post('/', validateShark, sharkController.createShark);
 router.put('/:id', validateShark, sharkController.updateShark);
 router.delete('/:id', sharkController.deleteShark);
