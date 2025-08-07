908b07b5ba61d87756b9a3eb836e98b6b3071d53
Your Nameelcapitan-g <gnewell89@gmail.com>
2025-08-07 11:54:36 -1000

    simoncarnal

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
-router.get('/', (req, res) => {
+//router.get('/', (req, res) => {
     //#sagger.tage['Hello Shark World']
-  res.send('Hello Shark World');
-});
+  //res.send('Hello Shark World');
+//});
 
 router.use('/sharks', require('./sharks'));
 router.use('/attacks', require('./attacks'));
